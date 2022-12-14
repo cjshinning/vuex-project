708efd6921484d99465dd7072f3906a1c772aede
cjshinningg <784319417@qq.com>
2022-12-14 15:19:08 +0800

    vuex中模块收集二

--- src/vuex/module/module-collection.js
+++ src/vuex/module/module-collection.js
@@ -1,4 +1,5 @@
 import { forEach } from '../util';
+import Module from './module';
 
 class ModuleCollection {
   constructor(options) {
@@ -7,18 +8,33 @@ class ModuleCollection {
     this.root = null;
     this.register([], options);
   }
-  register(path, rootModule) {
-    let newModule = {
-      _raw: rootModule,
-      _children: {},
-      state: rootModule.state
-    }
+  register(path, rawModule) {
+    let newModule = new Module(rawModule);
+
     if (path.length === 0) {
       this.root = newModule;
+    } else {
+      // [a] [a,c] [a,c,e,d]
+      // 找父亲
+      let parent = path.slice(0, -1).reduce((memo, current) => {
+        // return memo._children[current]
+        return memo.getChild(current);
+      }, this.root);
+
+      // parent._children[path[path.length - 1]] = newModule;
+      parent.addChild(path[path.length - 1], newModule);
+
+      // 根据当前注册的key，将他注册到对应的模块的儿子处
     }
-    if (rootModule.modules) {
-      forEach(rootModule.modules, (module, key) => {
-        console.log(module, key);
+    // 注册完毕当前模块，再进行注册跟模块
+    if (rawModule.modules) {
+      forEach(rawModule.modules, (module, key) => {
+        // console.log(module, key);
+        // []
+        // [a]
+        // [b]
+        // [a,c]
+        this.register(path.concat(key), module);
       })
     }
   }
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,6 +1,28 @@
-import { Vue } from './install';
-import { forEach } from './util';
 import ModuleCollection from './module/module-collection';
+
+function installModule(store, path, module) {
+  // 需要循环当前模块的
+  module.forEachGetter((fn, key) => {
+    store.getters[key] = function () {
+      return fn.call(store, module.state);
+    }
+  });
+  module.forEachMutation((fn, key) => {
+    store.mutations[key] = store.mutations[key] || [];
+    store.mutations[key].push((payload) => {
+      return fn.call(store, module.state, payload);
+    })
+  });
+  module.forEachAction((fn, key) => {
+    store.actions[key] = store.actions[key] || [];
+    store.actions[key].push((payload) => {
+      return fn.call(store, store, payload);
+    })
+  });
+  module.forEachChildren((child, key) => {
+    installModule(store, path.concat(key), child);
+  })
+}
 
 class Store {
   constructor(options) {
@@ -17,7 +39,16 @@ class Store {
     //   ]
     // }
 
-    let r = new ModuleCollection(options);  //对用户的参数进行格式化操作
+    this._module = new ModuleCollection(options);  //对用户的参数进行格式化操作
+    console.log(this._module);
+
+    this.getters = {};  //需要将模块中所有的getters，mutations，actions收集
+    this.mutations = {};
+    this.actions = {};
+
+    // 没有namespaced的时候getters都放在跟上，mutations，actions会被合并数组
+    installModule(this, [], this._module.root);
+    console.log(this.getters, this.mutations, this.actions);
   }
 
 }
